0961c2cde780c6cb0e6eaf021a153bffd6e8f9bc
조아빈 <whdkqls122@naver.com>
2025-05-23 17:23:29 +0900

    add : CategoryVO 추가

---
+++ src/main/java/domain/category/CategoryVO.java
@@ -0,0 +1,16 @@
+package domain.category;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CategoryVO {
+	private Long id;
+	private String name;
+}
