6df09305094b10bd4f67b0ffcf7b46bae383471a
조아빈 <whdkqls122@naver.com>
2025-05-25 01:26:21 +0900

    test : 카테고리 목록 조회 테스트 구현

--- src/test/java/dao/CategoryDaoImplTest.java
+++ src/test/java/dao/CategoryDaoImplTest.java
@@ -1,4 +1,40 @@
+package dao;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import database.JDBCUtil;
+import domain.CategoryVO;
+
 class CategoryDaoImplTest {
-  
+	@Test
+	@DisplayName("jdbc_ex에 접속한다.(자동 닫기)")
+	public void testConnection2() throws SQLException {
+		try(Connection conn = JDBCUtil.getConnection()){
+			System.out.println("DB 연결 성공");
+		}
+	}
+
+	@Test
+	@DisplayName("카테고리 목록을 조회 후 출력한다.")
+	void getCategories() {
+		CategoryDaoImpl dao = new CategoryDaoImpl();
+
+		List<CategoryVO> categoryVOList = dao.getCategories();
+
+		assertNotNull(categoryVOList);
+		assertFalse(categoryVOList.isEmpty());
+
+		for (CategoryVO category : categoryVOList) {
+			assertNotNull(category.getId());
+			assertNotNull(category.getName());
+			System.out.println(category.getId() + ". " + category.getName());
+		}
+	}
 }
