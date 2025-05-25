06d2ebcd9952fb7cde7fd3d8129bb143f931a084
조아빈 <whdkqls122@naver.com>
2025-05-25 21:07:08 +0900

    refactor : builder 생성자 수정 - 이전 category_id 없이 생성자를 생성하였지만 삽입 시 필요하여 @Builder를 클래스 단위로 설정

--- src/main/java/domain/food/FoodVO.java
+++ src/main/java/domain/food/FoodVO.java
@@ -8,17 +8,11 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class FoodVO {
 	private Long food_id;
 	private String name;
 	private int price;
 	private Long category_id;
-
-	@Builder
-	FoodVO(Long food_id, String name, int price) {
-		this.food_id = food_id;
-		this.name = name;
-		this.price = price;
-	}
 }
