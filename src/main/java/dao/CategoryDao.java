3472ec4b6a0346b550aacf674f09fe992271ca71
Minho Lee <3570js@naver.com>
2025-05-26 19:18:38 +0900

    Fix 오류부분 수정

--- src/main/java/dao/CategoryDao.java
+++ src/main/java/dao/CategoryDao.java
@@ -2,7 +2,7 @@ package dao;
 
 import java.util.List;
 
-import domain.CategoryVO;
+import domain.category.CategoryVO;
 
 public interface CategoryDao {
 	List<CategoryVO> getCategories();	//카테고리 목록 전체 조회
--- src/main/java/dao/CategoryDaoImpl.java
+++ src/main/java/dao/CategoryDaoImpl.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import config.JDBCUtil;
-import domain.CategoryVO;
+import domain.category.CategoryVO;
 
 public class CategoryDaoImpl implements CategoryDao{
 
