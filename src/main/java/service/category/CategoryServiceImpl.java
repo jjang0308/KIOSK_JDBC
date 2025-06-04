d18e825ba6ec20c40af3e17cd878c37cfce9ad2f
조아빈 <whdkqls122@naver.com>
2025-06-04 23:43:20 +0900

    feat : categoryservice 추가 - categoryDao를 통해 카테고리 목록을 출력 - 간격을 일정하게 띄우기 위해 CONSOLE_WIDTH 선언(검토 후 수정할 예정)

--- src/main/java/service/category/CategoryServiceImpl.java
+++ src/main/java/service/category/CategoryServiceImpl.java
@@ -1,2 +1,29 @@
-package service.category;public class CategoryServiceImpl {
+package service.category;
+
+import java.util.List;
+
+import dao.category.CategoryDao;
+import domain.category.CategoryVO;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService{
+	private final CategoryDao categoryDao;
+	private final int CONSOLE_WIDTH = 30;
+	@Override
+	public void printCategories() {
+		List<CategoryVO> categoryVOList =categoryDao.getCategories();
+		printCategories(categoryVOList);
+	}
+
+	private void printCategories(List<CategoryVO> categoryVOList) {
+		String info = "번호를 입력하면 선택됩니다.";
+		System.out.printf("카테고리 선택" + "%" + CONSOLE_WIDTH + "s\n", info);
+		System.out.println();
+		for (CategoryVO categoryVO : categoryVOList) {
+			System.out.println(categoryVO.getCategory_id() + "."  + categoryVO.getName());
+			System.out.println();
+		}
+		System.out.printf("6.뒤로가기" + "%" + CONSOLE_WIDTH + "s\n", "7.담은상품 결제하기");
+	}
 }
