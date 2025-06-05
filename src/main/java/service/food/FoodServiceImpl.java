13408f58af2a1f3f6a292702ba8014db566ed06d
조아빈 <whdkqls122@naver.com>
2025-06-06 01:08:12 +0900

    feat : food 목록 출력 서비스 추가

--- src/main/java/service/food/FoodServiceImpl.java
+++ src/main/java/service/food/FoodServiceImpl.java
@@ -3,23 +3,32 @@ package service.food;
 import java.util.List;
 
 import dao.food.FoodDaoImpl;
+import domain.category.CategoryVO;
 import domain.food.FoodVO;
 import lombok.RequiredArgsConstructor;
+import service.category.context.CategoryContext;
 
 @RequiredArgsConstructor
 public class FoodServiceImpl implements FoodService{
 	private final FoodDaoImpl foodDao;
+	private final CategoryContext categoryContext;
+	private final int CONSOLE_WIDTH = 30;
 	@Override
 	public void printFoodList() {
-		List<FoodVO> foodVOList = foodDao.getFoodListByCategory();
+		List<FoodVO> foodVOList = foodDao.getFoodListByCategory(categoryContext.categoryId());
+		printFoodList(foodVOList);
 	}
 
-	@Override
-	public void printFood() {
-
+	private void printFoodList(List<FoodVO> foodVOList){
+		String info = "번호를 입력하면 선택됩니다.";
+		long lastIdx = 0L;
+		System.out.printf("단품 선택" + "%" + CONSOLE_WIDTH + "s\n", info);
+		System.out.println();
+		for (FoodVO foodVO : foodVOList) {
+			System.out.println(foodVO.getCategory_id() + "."  + foodVO.getName() + "(" + foodVO.getPrice() + "원)");
+			System.out.println();
+			lastIdx = foodVO.getCategory_id();
+		}
+		System.out.println(lastIdx + 1 + ".뒤로가기");
 	}
-
-	// private void printFoodList(){
-	//
-	// }
 }
