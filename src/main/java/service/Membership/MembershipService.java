1c5a34369d937f4c2f2518908535bb7dba7379df
Minho Lee <3570js@naver.com>
2025-06-06 16:52:46 +0900

    Refactor membershipService 불필요한부분 삭제

--- src/main/java/service/Membership/MembershipService.java
+++ src/main/java/service/Membership/MembershipService.java
@@ -6,6 +6,5 @@ import java.util.List;
 public interface MembershipService {
     void joinMember(MembershipVO member);              // 회원 등록
     MembershipVO getMemberInfo(String phoneNum);       // 회원 정보 조회
-    void addPoint(String phoneNum, int point);         // 포인트 추가
-    List<MembershipVO> getAllMembers();                // 전체 회원 조회
+    void addPoint(String phoneNum, int point);         // 포인트 추가     // 전체 회원 조회
 }
