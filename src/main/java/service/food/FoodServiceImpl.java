694af11054e130c196e5190d68d8cf4881b29196
조아빈 <whdkqls122@naver.com>
2025-06-08 01:49:31 +0900

    refactor : FoodDao 타입 수정

--- src/main/java/service/food/FoodServiceImpl.java
+++ src/main/java/service/food/FoodServiceImpl.java
@@ -1,20 +1,16 @@
 package service.food;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
 
 import dao.food.FoodDao;
-import dao.food.FoodDaoImpl;
 import domain.food.FoodVO;
 import lombok.RequiredArgsConstructor;
-import service.Cart.CartService;
 import service.category.context.CategoryContext;
 
 @RequiredArgsConstructor
 public class FoodServiceImpl implements FoodService{
 	private final FoodDao foodDao;
-	private final CartService cartService;
 	private final CategoryContext categoryContext;
 	private final int CONSOLE_WIDTH = 30;
 	@Override
@@ -26,7 +22,7 @@ public class FoodServiceImpl implements FoodService{
 
 	private void printFoodList(List<FoodVO> foodVOList){
 		String info = "번호를 입력하면 선택됩니다.";
-		System.out.printf(categoryContext.getCategoryName() + " 선택" + "%" + CONSOLE_WIDTH + "s\n", info);
+		System.out.printf("단품 선택" + "%" + CONSOLE_WIDTH + "s\n", info);
 		System.out.println();
 		for (FoodVO foodVO : foodVOList) {
 			System.out.println(foodVO.getCategory_id() + "."  + foodVO.getName() + "(" + foodVO.getPrice() + "원)");
@@ -41,14 +37,14 @@ public class FoodServiceImpl implements FoodService{
 			try {
 				String input = sc.nextLine();
 				Long inputFoodId = Long.parseLong(input);
-				Optional<FoodVO> matched = foodVOList.stream()
-					.filter(vo -> vo.getFood_id().equals(inputFoodId))
-					.findFirst();
-				if (matched.isEmpty()) {
+
+				boolean exists = foodVOList.stream()
+					.anyMatch(vo -> vo.getFood_id().equals(inputFoodId));
+				if (!exists) {
 					System.out.println("존재하지 않는 음식 번호입니다. 다시 입력해주세요.");
 					continue;
 				}
-				cartService.addToCart(matched.get());
+				//장바구리 리스트에 추가 하는 로직 추가
 				break;
 			} catch (NumberFormatException e) {
 				System.out.println("숫자만 입력해야 합니다.");
