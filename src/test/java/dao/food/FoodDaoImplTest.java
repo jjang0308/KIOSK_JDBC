5a80bc011d24717f92d3db4ebb6d2440a43c3f19
조아빈 <whdkqls122@naver.com>
2025-06-08 17:28:05 +0900

    test : insert 로직 삭제

--- src/test/java/dao/food/FoodDaoImplTest.java
+++ src/test/java/dao/food/FoodDaoImplTest.java
@@ -3,10 +3,6 @@ package dao.food;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -18,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 class FoodDaoImplTest {
-	private FoodDao foodDao = new FoodDaoImpl();
+	private final FoodDao foodDao = new FoodDaoImpl();
 	Connection conn = JDBCUtil.getConnection();
 
 	@Test
@@ -35,8 +31,7 @@ class FoodDaoImplTest {
 			.price(6000)
 			.category_id(categoryId)
 			.build();
-		foodDao.insert(food1);
-		foodDao.insert(food2);
+
 
 		List<FoodVO> foodVOList = foodDao.getFoodListByCategory(categoryId);
 		assertNotNull(foodVOList);
@@ -55,7 +50,6 @@ class FoodDaoImplTest {
 			.category_id(foodId)
 			.build();
 
-		foodDao.insert(food1);
 		FoodVO foodVO = foodDao.getFood(foodId);
 		assertNotNull(foodVO);
 		assertEquals("불고기버거", foodVO.getName());
